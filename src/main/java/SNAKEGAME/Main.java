88272027391eaa9bbee80ff11e997152807559c7
lavanya2806 <lavanya1414.cse18@chitkara.edu.in>
2021-03-24 12:18:32 +0530

    snakegame

---
+++ src/main/java/SNAKEGAME/Main.java
@@ -0,0 +1,26 @@
+package SNAKEGAME;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame();
+		GamePlay gameplay = new GamePlay();
+		
+		obj.setBounds(10, 10, 905, 700);
+		obj.setBackground(Color.darkGray);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+
+		
+	}
+
+}
+
